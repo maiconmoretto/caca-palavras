0ee91479d8439782ba624ffeb041225257006b6f
maiconmoretto <maiconmorettos@gmail.com>
2017-09-11 21:05:56 -0300

    adicionado projeto

---
+++ src/caça/palavras/CaçaPalavras.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caça.palavras;
+
+/**
+ *
+ * @author 
+ * 
+ * descricao:
+ * Implementar um algoritmo que crie um tabuleiro de jogo de "Caça Palavras".
+
+Tarefas
+
+Para a obtenção do conceito C, são necessárias as seguintes funcionalidades:
+
+Ler a partir de um arquivo uma lista de palavras, uma por linha.
+Gerar um grid com as palavras carregadas, na horizontal, da esquerda para a direita, ou na vertical, de cima para baixo. Nas posições em que não se encontram letras das palavras, inserir letras aleatórias.
+Fazer com que existam cruzamentos entre as palavras. Pelo menos 3 cruzamentos simples, onde uma palavra cruza uma outra, e um cruzamento duplo, onde uma palavra cruza duas outas.
+As palavras não podem estar em linhas ou colunas adjacentes no grid.
+ * 
+ */
+public class CaçaPalavras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
